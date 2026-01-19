e35301b93609970cdc62b99c2d6f39d2de14858b
yanakmrv <ikmrv9@gmail.com>
2026-01-20 00:09:28 +0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,22 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Ошибка: деление на ноль"
+    return a / b
+
+if __name__ == "__main__":
+    # 
+  Пример использования
+    print("Калькулятор")
+    print(f"2 + 3 = {add(2, 3)}")
+    print(f"5 - 2 = {subtract(5, 2)}")
+    print(f"4 * 3 = {multiply(4, 3)}")
+    print(f"10 / 2 = {divide(10, 2)}")
